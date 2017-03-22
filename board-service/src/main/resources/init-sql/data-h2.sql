2829d1b30ef350944eadf1752b19c4ed7fe633fa
sshyun33 <sshyun33@gmail.com>
2017-03-22 09:29:47 +0900

    + 스프링 프로파일 적용 + 개발 환경 시 sql 초기화 데이터 자동 생성  + gradle wrapper 폴더 추가

---
+++ board-service/src/main/resources/init-sql/data-h2.sql
@@ -0,0 +1,11 @@
+INSERT INTO TBL_BOARD(BOARD_TITLE, BOARD_DESCRIPTION, BOARD_STATUS, OPEN_DATE, CLOSE_DATE) VALUES
+ ('title-01', 'desc-01', 'OPEN', CURRENT_DATE(), '9999-01-01'),
+ ('title-02', 'desc-02', 'OPEN', CURRENT_DATE(), '9999-01-01'),
+ ('title-03', 'desc-03', 'OPEN', CURRENT_DATE(), '9999-01-01'),
+ ('title-04', 'desc-04', 'OPEN', CURRENT_DATE(), '9999-01-01'),
+ ('title-05', 'desc-05', 'OPEN', CURRENT_DATE(), '9999-01-01'),
+ ('title-06', 'desc-06', 'OPEN', CURRENT_DATE(), '9999-01-01'),
+ ('title-07', 'desc-07', 'OPEN', CURRENT_DATE(), '9999-01-01'),
+ ('title-08', 'desc-08', 'OPEN', CURRENT_DATE(), '9999-01-01'),
+ ('title-09', 'desc-09', 'OPEN', CURRENT_DATE(), '9999-01-01'),
+ ('title-10', 'desc-10', 'OPEN', CURRENT_DATE(), '9999-01-01');
